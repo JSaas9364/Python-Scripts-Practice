a0dc58a3987dc282166c7b852e1dc3895a64a828
Juan <46148241+JSaas9364@users.noreply.github.com>
2025-02-27 20:06:08 -0500

    Update SchoolLabs3.py

--- ProjectsLearning/SchoolLabs3.py
+++ ProjectsLearning/SchoolLabs3.py
@@ -1,5 +1,4 @@
-t2 = (1, 2, 3, 4, 5)
-*first, middle, last = t2
-print(first)   # Output: 1
-print(middle)  # Output: [2, 3, 4]
-print(last)    # Output: 5
+x = {'d': '400', 'e': '500', 'f': '600'}
+y= {'celery': '2.81', 'milk': '4.34', 'bread': '5.63'}
+
+print((len(x), len(y)), (x, y))
