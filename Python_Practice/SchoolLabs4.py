abc403df3f4b897572cbcb40dc4e073552258363
Juan <46148241+JSaas9364@users.noreply.github.com>
2025-03-28 18:47:08 -0400

    update

--- Python_Practice/SchoolLabs4.py
+++ Python_Practice/SchoolLabs4.py
@@ -1,7 +1,18 @@
-def generator(start,stop):
-    while (start<=stop):
-        yield start
-        print(f'start={start}')
-        start+=1
-for counter in generator(3,4):
-    print(f'counter={counter}')   
+def count_word_frequency(words):
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
+
+
+with open("nameslist.txt", "r") as f:
+    words = f.read().splitlines()
+    
+# call function
+frequency = count_word_frequency(words)
+print(frequency)
+
+from collections import Counter
+
+for word, count in frequency.items():
+    print(f"{word}: {count}")
